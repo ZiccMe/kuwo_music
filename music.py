a7c998977417d7645b27bd82354bb5419429459c
ZiccMe <65123587+ZiccMe@users.noreply.github.com>
2023-04-11 18:53:41 +0800

    Update music.py

--- music.py
+++ music.py
@@ -11,16 +11,11 @@ import urllib3
 #https://m.kuwo.cn/h5app/playlist/3302085519?t=plantform&from=ar
 pid = 3302085519  #分享歌单时选择 复制试听链接 位于 playlist/ 之后的一串数字
 page = 1
-
-
 while 1:
-    url = 'https://m.kuwo.cn/newh5app/wapi/api/www/playlist/playListInfo?pid=' + str(pid) + '&pn=' + str(
-        page) + '&rn=100'
+    url = 'https://m.kuwo.cn/newh5app/wapi/api/www/playlist/playListInfo?pid=' + str(pid) + '&pn=' + str(page) + '&rn=100'
     page += 1
-
     http = urllib3.PoolManager()
     json_file = http.request('GET', url, headers={'Accept': 'application/json'})
-
     json_dict: dict = json.loads(json_file.data.decode('utf-8'))
     if len(json_dict['data']['musicList']) == 0 :
         print("\n\n【所有歌曲搜寻完毕】")
